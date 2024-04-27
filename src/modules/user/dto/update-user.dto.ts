718cdcc73e02cd116d8ac67e23812e569f021e14
Charles <charleslanop@gmail.com>
2024-04-27 15:54:50 -0300

    updated

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,8 +1,11 @@
-import { IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, Matches, MaxLength, Min, MinLength } from 'class-validator';
 import { UserDto } from './user.dto';
 
 export class UpdateUserDto {
   @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(15)
+  @Matches(/^[a-zA-Z0-9]*$/, { message: 'O nome deve conter apenas letras e números' })
   name: string;
 
   @IsNumber()
@@ -15,6 +18,8 @@ export class UpdateUserPasswordDto extends UserDto {
   currentPassword: string;
 
   @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(30)
   newPassword: string;
 
   @IsNumber()
